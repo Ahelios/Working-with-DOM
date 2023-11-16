dba5850b5b992b55e7d2e613b4032820b243a2e9
Ahelios <artursmedveds@gmail.com>
2023-11-16 17:26:13 +0200

    Finalised without Extra

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,74 +1,70 @@
 import $ from 'jquery';
 import sum from './utils/sum/sum';
 
-console.log('Ready for coding');
-
-console.log('Body jQuery node:', $('body'));
-console.log('Body javascript node:', document.querySelector('body'));
-console.log('2 + 3 =', sum(2, 3));
-
-//================================ ON CLICK ACTIONS===============================
-
 // Button 1 -----------------------------------------------------------------------
-const newBackgroundColor = document.querySelector('.button1') as HTMLElement;
+const button1 = document.querySelector<HTMLButtonElement>('.button1');
+const square1 = document.querySelector<HTMLElement>('.square1');
 let isYellow = false;
 
-function changeColor() {
+const changeColor = () => {
   if (isYellow) {
     // If the background is yellow, change it back to its original color
-    newBackgroundColor.style.backgroundColor = ''; // Set to '' to reset to CSS value
+    square1.style.backgroundColor = ''; // Set to '' to reset to CSS value
     isYellow = false;
   } else {
     // If the background is not yellow, change it to yellow
-    newBackgroundColor.style.backgroundColor = 'yellow';
+    square1.style.backgroundColor = 'yellow';
     isYellow = true;
   }
-}
+};
 
-newBackgroundColor.addEventListener('click', changeColor);
+button1.addEventListener('click', changeColor);
 
 // Button 2 -----------------------------------------------------------------------
-const newTextSuccess = document.querySelector('.bt2__text') as HTMLElement;
+const button2 = document.querySelector<HTMLButtonElement>('.button2') as HTMLElement;
+const square2Text = document.querySelector<HTMLElement>('.square2-text') as HTMLElement;
 let isSuccess = false;
 
-function changeToSuccess() {
+const changeToSuccess = () => {
   if (isSuccess) {
     // If the text is success
-    newTextSuccess.textContent = 'Button 2'; // Set to '' to reset to original text
+    square2Text.textContent = 'FAIL'; // Set to '' to reset to original text
     isSuccess = false;
   } else {
     // If the text is not 'success', change it to original text
-    newTextSuccess.textContent = 'SUCCESS';
+    square2Text.textContent = 'SUCCESS';
     isSuccess = true;
   }
-}
+};
 
-newTextSuccess.addEventListener('click', changeToSuccess);
+button2.addEventListener('click', changeToSuccess);
 
 // Button 3 -----------------------------------------------------------------------
-const invisibleButton = document.querySelector<HTMLButtonElement>('.button3');
+const button3 = document.querySelector<HTMLButtonElement>('.button3');
+const square3 = document.querySelector<HTMLElement>('.square3');
 
-function changeToInvisible() {
-  invisibleButton.style.visibility = 'hidden';
+const changeToInvisible = () => {
+  square3.style.visibility = 'hidden';
   setTimeout(() => {
-    invisibleButton.style.visibility = 'visible';
+    square3.style.visibility = 'visible';
   }, 2000); // 2000 milliseconds = 2 seconds
-}
+};
 
-invisibleButton.addEventListener('click', changeToInvisible);
+button3.addEventListener('click', changeToInvisible);
 
 // Button 4 -----------------------------------------------------------------------
 const button4 = document.querySelector<HTMLButtonElement>('.button4');
+const square4 = document.querySelector<HTMLButtonElement>('.square4');
 const emptyContainer = document.querySelector<HTMLElement>('.empty_container');
 
 let isInvisible = false;
 
 const changeToInvisible2 = () => {
   if (isInvisible) {
-    button4.style.visibility = 'visible';
+    square4.style.visibility = 'visible';
     isInvisible = false;
   } else {
-    button4.style.visibility = 'hidden';
+    square4.style.visibility = 'hidden';
     emptyContainer.style.display = 'block';
     isInvisible = true;
   }
@@ -79,27 +75,72 @@ emptyContainer.addEventListener('click', changeToInvisible2);
 
 // Button 5 -----------------------------------------------------------------------
 const button5 = document.querySelector<HTMLButtonElement>('.button5');
+const square5 = document.querySelector<HTMLElement>('.square5');
 
 const predifinedColors = ['red', 'black', 'aqua', 'brown', 'orange'];
 
 const changeToFiveColors = () => {
   const randomIndex = Math.floor(Math.random() * predifinedColors.length)
 
-  button5.style.backgroundColor = predifinedColors[randomIndex];
+  square5.style.backgroundColor = predifinedColors[randomIndex];
 };
 
 button5.addEventListener('click', changeToFiveColors);
 
-// Button 5 -----------------------------------------------------------------------
+// Button 6 -----------------------------------------------------------------------
 const button6 = document.querySelector<HTMLButtonElement>('.button6');
-const button6_number = document.querySelector<HTMLElement>('button6_number');
+const square6 = document.querySelector<HTMLElement>('.square6');
+let intervalIdentifier: number | undefined;
 
-const increaseNumber = () => {
-  let number = Number(button6_number);
-
-  for (let i = 6; i <= 10; i++) {
-    number = number + 1;
+const startIncreasingNumber = () => {
+  if (intervalIdentifier !== undefined) {
+    clearInterval(intervalIdentifier);
   }
+
+  let currentNumber = 0;
+  square6.textContent = `${currentNumber}`;
+  
+  intervalIdentifier = window.setInterval(() => {
+    currentNumber = currentNumber + 1;
+    if (currentNumber > 10) {
+      currentNumber = 0;
+    }
+    square6.textContent = `${currentNumber}`;
+  }, 3000);
 }
 
-button6.addEventListener('click', increaseNumber);
+button6.addEventListener('click', startIncreasingNumber);
+
+// Button 7 -----------------------------------------------------------------------
+const button7 = document.querySelector<HTMLButtonElement>('.button7');
+const allSquares = document.querySelector<HTMLButtonElement>('.square');
+const bigBackground = document.querySelector<HTMLButtonElement>('.white-square');
+
+let isNewColors = false;
+
+const changeColorAll = () => {
+  if (isNewColors) {
+    // If the background is yellow, change it back to its original color
+    square1.style.backgroundColor = ''; // Set to '' to reset to CSS value
+    bigBackground.style.backgroundColor = '';
+    isNewColors = false;
+  } else {
+    // If the background is not yellow, change it to yellow
+    allSquares.style.backgroundColor = '#18D5E1';
+    bigBackground.style.backgroundColor = '#000000';
+    isNewColors = true;
+  }
+};
+
+button7.addEventListener('click', changeColorAll);
+// Square 1 -----------------------------------------------------------------------
+const changeColorRed = () => {
+  square1.style.backgroundColor = 'red';
+};
+
+const returnColor = () => {
+  square1.style.backgroundColor = '';
+};
+
+square1.addEventListener('mouseover', changeColorRed);
+square1.addEventListener('mouseout', returnColor);
